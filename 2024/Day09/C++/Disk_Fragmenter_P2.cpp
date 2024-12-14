7d9fbd46a9a18a9d48d35aa4f4ca898d7b7d181d
whoismtrx <orekab3@gmail.com>
2024-12-14 20:22:05 +0100

    Solutions for Day09 with C++

--- 2024/Day09/C++/Disk_Fragmenter_P2.cpp
+++ 2024/Day09/C++/Disk_Fragmenter_P2.cpp
@@ -7,42 +7,35 @@
 
 using namespace std;
 
-void fun(map<int, pair<bool,pair<int,int>>, greater<int>> &mp, set<pair<int,int>> &se)
+long long fun(map<int, pair<int,int>, greater<int>> &mp, map<int, set<int>> &se)
 {
-    set<int, greater<int>> keys;
-    pair<int,int>pr;
-    for (auto it = mp.begin(); it != mp.end(); it++)
-        keys.insert(it->first);
-    int key = *keys.begin();
-    for (auto it = mp.find(key); it != mp.end();)
+    long long ret = 0;
+
+    while (mp.size())
     {
-        bool b = false;
-        if (it->second.first)
+        int idx = 1e9;
+        auto it = mp.begin();
+        auto jt = se.begin();
+        for (auto kt = jt, nkt = kt; kt != se.end(); nkt++, kt = nkt)
         {
-            it++;
-            continue;
+            if (!kt->second.size())
+                se.erase(kt);
+            else if (kt->first >= it->second.first && *kt->second.begin() < idx && *kt->second.begin() < it->first)
+                idx = *kt->second.begin(), jt = kt;
         }
-        for (auto jt = se.begin(); jt->first < it->first; jt++)
+        if (idx != 1e9)
         {
-            if (jt->second >= it->second.second.first)
-            {
-                b = true;
-                pr = {jt->first + it->second.second.first, jt->second-it->second.second.first};
-                mp.insert({jt->first,{true,it->second.second}});
-                se.insert(pr);
-                se.erase(jt);
-                se.insert({it->first, it->second.second.first});
-                break;
-            }
+            jt->second.erase(jt->second.begin());
+            se[jt->first - it->second.first].insert(idx + it->second.first);
+            for (int i = 0; i < it->second.first; i++)
+                ret += it->second.second * (idx + i);
         }
-        if (!b)
-            it->second.first = true;
         else
-            mp.erase(it);
-        keys.erase(keys.begin());
-        key = *keys.begin();
-        it = mp.find(key);
+            for (int i = 0; i < it->second.first; i++)
+                ret += it->second.second * (it->first + i);
+        mp.erase(it);
     }
+    return ret;
 }
 
 int main()
@@ -53,38 +46,21 @@ int main()
 
     string str;
     long long ret = 0, id = 0, idx = 0;
-    map<int, pair<bool,pair<int,int>>, greater<int>> mp;
-    set<pair<int,int>> se;
+    map<int, pair<int,int>, greater<int>> mp;
+    map<int, set<int>> se;
 
     getline(cin, str);
     for(int i = 0; i < str.size(); i++)
     {
-        if (i % 2)
-            se.insert({idx,str[i]-48});
-        else
-            mp.insert({idx,{false,{str[i]-48,id++}}});
+        if (str[i] - 48)
+        {
+            if (i % 2)
+                se[str[i]-48].insert(idx);
+            else
+                mp.insert({idx,{str[i]-48,id++}});
+        }
         idx += str[i] - 48;
     }
-    fun(mp, se);
-    idx = 0;
-    auto st = se.begin();
-    for (auto it = mp.rbegin(); it != mp.rend(); it++)
-    {
-        while (st->first < it->first)
-        {
-            id = st->second;
-            while (id--)
-                idx++;
-            se.erase(st);
-            st = se.begin();
-        }
-        id = it->second.second.first;
-        while (id--)
-        {
-            ret += it->second.second.second * idx;
-            idx++;
-        }   
-    }
-    cout << ret << endl;
+    cout << fun(mp, se) << endl;
     return 0;
 }
