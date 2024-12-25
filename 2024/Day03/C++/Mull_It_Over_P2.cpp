807699113d01ff56c81329a95e6517f28912a46c
Mohcine-Ghalmi <81354228+Mohcine-Ghalmi@users.noreply.github.com>
2024-12-25 19:48:29 +0100

    Update Mull_It_Over_P2.cpp
    optimized code using regex

--- 2024/Day03/C++/Mull_It_Over_P2.cpp
+++ 2024/Day03/C++/Mull_It_Over_P2.cpp
@@ -1,91 +1,42 @@
-//MTRX
-
+//SLEEPS
 #include <iostream>
-#include <sstream>
+#include <fstream>
 #include <string>
-#include <map>
+#include <regex>
+#include <sstream>
 
-using namespace std;
+int calculateConditionalMultiplications(const std::string& fileContent) {
+	std::regex instructionPattern(R"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))");
+	std::smatch match;
+	std::string::const_iterator searchStart(fileContent.cbegin());
 
-bool isValid(string &s)
-{
-    bool b = true;
-    for(int i = 0; i < s.size(); i++)
-    {
-        if (!isdigit(s[i]) && s[i] != ',')
-            return false;
-        if (s[i] == ',' && !b)
-            return false;
-        if (s[i] == ',')
-            b = false;
-    }
-    return true;
-}
+	bool isEnabled = true; 
+	int totalSum = 0;
 
-long long mul(string &s, bool b)
-{
-    long long a = stol(s.substr(0, s.find(','))), c = stol(s.substr(s.find(',')+1));
-    if (!b)
-        a = 0;
-    return a*c;
+	while (std::regex_search(searchStart, fileContent.cend(), match, instructionPattern)) {
+		std::string instruction = match[0];
+		if (instruction == "do()")
+			isEnabled = true;
+		else if (instruction == "don't()")
+			isEnabled = false;
+		else if (match[1].matched && match[2].matched)
+			if (isEnabled) {
+				int x = std::stoi(match[1].str());
+				int y = std::stoi(match[2].str());
+				totalSum += x * y;
+			}
+		searchStart = match.suffix().first;
+	}
+	return (totalSum);
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    long long ret = 0;
-    string str, tmp;
-    getline(cin, str, '\0');
-    size_t i = 0, j = 0, e = 0;
-    map<int,string> mp;
-    mp.insert({0, "do()"});
-    while (true)
-    {
-        i = str.find("mul", i);
-        if (i == str.npos)
-            break;
-        i += 3;
-        if (str[i] != '(')
-            continue;
-        e = str.find_first_of(')', i);
-        if (e == str.npos)
-            continue;
-        i++;
-        tmp = str.substr(i, e-i);
-        if (!isValid(tmp))
-            continue;
-        mp.insert({i,tmp});
-    }
-    i = 0, j = 0;
-    while (true)
-    {
-        i = str.find("don't()", i);
-        j = str.find("do()", j);
-        if (i != str.npos)
-            mp.insert({i, str.substr(i,7)}), i++;
-        if (j != str.npos)
-            mp.insert({j, str.substr(j,4)}), j++;
-        if (i == str.npos && j == str.npos)
-            break;
-    }
-    bool b = true;
-    for (auto it = mp.begin(); it != mp.end(); it++)
-    {
-        if (it->second == "don't()")
-        {
-            b = false;
-            continue;
-        }
-        else if (it->second == "do()")
-        {
-            b = true;
-            continue;
-        }
-        ret += mul(it->second, b);
-    }
-    cout << ret << endl;
-    return 0;
+    std::ifstream fileInput("test.txt");
+	std::stringstream buffer;
+	buffer << fileInput.rdbuf();
+	std::string fileContent = buffer.str();
+	int result = calculateConditionalMultiplications(fileContent);
+	std::cout << "Sum of enabled multiplications: " << result << std::endl;
+	return (0);
 }
